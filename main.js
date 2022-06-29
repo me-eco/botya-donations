0bc35d49c528ea02d31bae8cba5e781d80910349
Denis Voitenko <denis@denvot.dev>
2022-06-29 10:58:18 +0300

    Fix Qiwi Id validation

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let sum = NaN;
 
 let isLightTheme = true;
 
-if(qiwiId === null || qiwiId.length !== 11)
+if(qiwiId === null)
 {
     let nowQiwiScreen = document.querySelector(".no-qiwi-id-error");
     let mainWrapper = document.querySelector(".main-wrapper");
